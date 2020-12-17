9d417d46d8375fe91dc47bcc08636e2c56225cc1
Jordi <jordi.bravo.bad@gmail.com>
2020-12-17 17:33:09 -0300

    Adding Components

--- Source/ToonTanks/Pawns/Tank.cpp
+++ Source/ToonTanks/Pawns/Tank.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Tank.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SceneComponent.h"
 
 // Sets default values
 ATank::ATank()
@@ -9,6 +11,15 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Declare components by sub-objects
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base"));
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret"));
+	ProjectileSpawnPoint = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ProjectileSpawnPoint"));
+
+	// Root and attachments
+	SetRootComponent(CapsuleComp);
+	BaseMesh->SetupAttachment(CapsuleComp);
 }
 
 // Called when the game starts or when spawned
